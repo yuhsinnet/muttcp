daf76cf7c04fcb48beb162690252f45a3494a3a8
x78701542000 <x78701542000@livemail.tw>
2019-05-25 09:32:05 +0800

    創建

---
+++ MUTTCP/MUTTCP_ss/Form1.Designer.vb
@@ -0,0 +1,114 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TGPort = New System.Windows.Forms.TextBox()
+        Me.MYPort = New System.Windows.Forms.TextBox()
+        Me.AutoMode = New System.Windows.Forms.CheckBox()
+        Me.Start_Button = New System.Windows.Forms.Button()
+        Me.STOP_Button = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("新細明體", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(63, 44)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(456, 34)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "目標設備端口  <->    本幾端口"
+        '
+        'TGPort
+        '
+        Me.TGPort.Font = New System.Drawing.Font("新細明體", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.TGPort.Location = New System.Drawing.Point(69, 114)
+        Me.TGPort.Name = "TGPort"
+        Me.TGPort.Size = New System.Drawing.Size(201, 47)
+        Me.TGPort.TabIndex = 1
+        '
+        'MYPort
+        '
+        Me.MYPort.Font = New System.Drawing.Font("新細明體", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.MYPort.Location = New System.Drawing.Point(362, 114)
+        Me.MYPort.Name = "MYPort"
+        Me.MYPort.Size = New System.Drawing.Size(201, 47)
+        Me.MYPort.TabIndex = 2
+        '
+        'AutoMode
+        '
+        Me.AutoMode.AutoSize = True
+        Me.AutoMode.Font = New System.Drawing.Font("新細明體", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.AutoMode.Location = New System.Drawing.Point(580, 44)
+        Me.AutoMode.Name = "AutoMode"
+        Me.AutoMode.Size = New System.Drawing.Size(173, 28)
+        Me.AutoMode.TabIndex = 3
+        Me.AutoMode.Text = "自動啟動模式"
+        Me.AutoMode.UseVisualStyleBackColor = True
+        '
+        'Start_Button
+        '
+        Me.Start_Button.Font = New System.Drawing.Font("新細明體", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Start_Button.Location = New System.Drawing.Point(580, 114)
+        Me.Start_Button.Name = "Start_Button"
+        Me.Start_Button.Size = New System.Drawing.Size(124, 47)
+        Me.Start_Button.TabIndex = 4
+        Me.Start_Button.Text = "啟動"
+        Me.Start_Button.UseVisualStyleBackColor = True
+        '
+        'STOP_Button
+        '
+        Me.STOP_Button.Font = New System.Drawing.Font("新細明體", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.STOP_Button.Location = New System.Drawing.Point(580, 167)
+        Me.STOP_Button.Name = "STOP_Button"
+        Me.STOP_Button.Size = New System.Drawing.Size(124, 47)
+        Me.STOP_Button.TabIndex = 5
+        Me.STOP_Button.Text = "暫停"
+        Me.STOP_Button.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.STOP_Button)
+        Me.Controls.Add(Me.Start_Button)
+        Me.Controls.Add(Me.AutoMode)
+        Me.Controls.Add(Me.MYPort)
+        Me.Controls.Add(Me.TGPort)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "雙伺服多工"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TGPort As TextBox
+    Friend WithEvents MYPort As TextBox
+    Friend WithEvents AutoMode As CheckBox
+    Friend WithEvents Start_Button As Button
+    Friend WithEvents STOP_Button As Button
+End Class
---
+++ MUTTCP/MUTTCP_ss/MTTCP.vb
@@ -0,0 +1,285 @@
+﻿
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Imports System.Timers
+
+Public Class MTTCP
+
+    Public Delegate Sub PrintTextDG(ByVal Text As String)
+    Public Event PrintText As PrintTextDG
+
+    Const ClientNumber As Integer = 3
+
+    Dim TCPs0 As TcpListener
+    Dim TCPs0c As TcpClient
+
+    Dim TCPc_Stream As NetworkStream
+
+    Dim TCPs As TcpListener
+
+    Dim TCPsc(ClientNumber) As TcpClient
+    Dim TCPscThread(ClientNumber) As Thread
+    Dim TCPscIdleTime(ClientNumber) As Integer
+
+    Public TCPsQueue As New Queue
+    Public Class TCPsQueueCSS
+
+        Public index As Integer
+        Public str As String
+
+    End Class
+    Dim sendTime As Integer
+    Public Sub New(TGPort As Integer, MYBindPort As Integer)
+
+        '初始化客戶端連線
+        Dim TCPs0Thread As New Thread(AddressOf StartTCPs0Listen)
+        TCPs0Thread.IsBackground = True
+        TCPs0Thread.Start(TGPort)
+
+        Dim TCPsThread As New Thread(AddressOf StartTCPsListen)
+        TCPsThread.IsBackground = True
+        TCPsThread.Start(MYBindPort)
+
+        Dim CoreThread As New Thread(AddressOf CorePress)
+        CoreThread.IsBackground = True
+        CoreThread.Start()
+
+
+
+    End Sub
+
+    Private Sub StartTCPs0Listen(ByVal state As Object)
+
+        Dim iPort As Integer
+        iPort = CType(state, Integer)
+
+        TCPs0 = New TcpListener(IPAddress.Any, iPort)
+        TCPs0.Start()
+
+        TCPs0c = TCPs0.AcceptTcpClient
+
+
+
+        RaiseEvent PrintText("TCPsc is connect >.< " & "  TCPs0" & "     ")
+
+
+    End Sub
+
+    Private Sub CorePress()
+
+        While True
+
+            If TCPsQueue.Count > 0 Then
+
+                RaiseEvent PrintText("Q Get  >.< ")
+                Dim te As TCPsQueueCSS = TCPsQueue.Dequeue()
+                Dim send_buf As String
+
+                send_buf = TCPc_Write(te.str)
+
+                Dim send_Bytes() = Text.Encoding.ASCII.GetBytes(send_buf)
+                TCPsc(te.index).GetStream.Write(send_Bytes, 0, send_Bytes.Length)
+                RaiseEvent PrintText("TCPsc is Transmit  >.< " & "  TCPsc(" & te.index & ")     DATA:(" & send_buf & ")")
+                Thread.Sleep(10)
+            Else
+
+                Thread.Sleep(10)
+
+            End If
+
+
+        End While
+
+
+    End Sub
+
+    Private Sub StartTCPsListen(ByVal state As Object)
+        Dim iPort As Integer
+        iPort = CType(state, Integer)
+
+        TCPs = New TcpListener(IPAddress.Any, iPort)
+        TCPs.Start()
+
+        Do
+            For index = 0 To (TCPsc.Length - 1)
+
+                If TCPsc(index) Is Nothing Then
+
+
+                    TCPsc(index) = TCPs.AcceptTcpClient
+                    TCPscThread(index) = New Thread(AddressOf TCPscReceive)
+                    TCPscThread(index).IsBackground = True
+                    TCPscThread(index).Start(index)
+
+
+                    RaiseEvent PrintText("TCPsc is connect >.< " & "  TCPsc(" & index & ")     ")
+
+
+
+                    Exit For
+
+                End If
+
+            Next
+
+
+
+        Loop
+
+
+
+
+    End Sub
+
+    Private Sub TCPscReceive(ByVal state As Object)
+
+        Const UPTime As Integer = 200 'x 100ms
+
+        Dim index As Integer = CType(state, Integer)
+
+        Dim Read_str As String
+
+        Dim TCPSendTime As New System.Threading.Timer(AddressOf TCPscSendTimeUp, index, 0, 100)
+
+
+        While TCPscIdleTime(index) < UPTime
+
+            Try
+                If TCPsc(index).Available > 3 Then
+
+                    Dim Read_buf(1024) As Byte
+                    TCPsc(index).GetStream.Read(Read_buf, 0, Read_buf.Length)
+                    Read_str = System.Text.Encoding.ASCII.GetString(Read_buf)
+
+                    Dim Read_str_remov As String
+                    Read_str_remov = RemoveNullChar(Read_str)
+
+
+                    Dim temp As New TCPsQueueCSS
+                    temp.index = index
+                    temp.str = Read_str_remov
+
+                    TCPsQueue.Enqueue(temp)
+
+                    RaiseEvent PrintText("TCPsc is Receive >.< " & "  TCPsc(" & index & ")     DATA:(" & Read_str_remov & ")")
+                    TCPscIdleTime(index) = 0
+                Else
+
+                    Thread.Sleep(1)
+
+                End If
+            Catch ex As Exception
+
+
+
+                Exit While
+            End Try
+
+
+
+
+
+
+        End While
+
+        RaiseEvent PrintText("TCPsc is Disconnect >.< " & "  TCPsc(" & index & ")")
+        TCPSendTime.Dispose()
+        TCPsc(index).Close()
+        TCPsc(index) = Nothing
+        TCPscThread(index).Abort()
+        TCPscThread(index) = Nothing
+
+    End Sub
+
+    Private Sub TCPscSendTimeUp(state As Object)
+
+        Dim index As Integer = CInt(state)
+
+        TCPscIdleTime(index) = TCPscIdleTime(index) + 1
+
+
+    End Sub
+
+
+
+    Private Shared Function RemoveNullChar(In_str As String) As String
+
+
+        Return In_str.Remove(In_str.IndexOf(vbNullChar))
+
+    End Function
+
+    Private Sub TCPcSendTimeUp(state As Object)
+
+        sendTime += 1
+
+    End Sub
+    Public Function TCPc_Write(SendData As String) As String
+        '
+        ' SendData:
+        ' 以字串的方式傳輸數據
+
+        Const UPTime As Integer = 200 'x 100ms
+
+        Dim SendBuf As Byte() = Text.ASCIIEncoding.ASCII.GetBytes(SendData)
+
+        TCPc_Stream.Write(SendBuf, 0, SendBuf.Length)
+
+        '
+        '
+        Dim TCPScendTime As New Threading.Timer(AddressOf TCPcSendTimeUp, vbNull, 0, 100) '100ms
+        sendTime = 0
+
+        '
+        '
+        Dim ReadIndex As Integer = 0
+
+        Dim ReturnBuf As String
+        Dim ReadLen As Integer
+        Dim DetETX As Integer
+        Dim Read_buf(1024) As Byte
+        While sendTime < UPTime
+            Thread.Sleep(5)
+
+
+
+            ReadLen = TCPs0c.Available
+            TCPc_Stream.Read(Read_buf, ReadIndex, ReadLen)
+
+
+            For DetETX = 0 To ReadIndex + ReadLen + 10
+
+                If Read_buf(DetETX) = 3 Then
+                    sendTime = 0
+
+                    ReturnBuf = System.Text.Encoding.ASCII.GetString(Read_buf).Remove(DetETX + 1)
+                    Array.Clear(Read_buf, 0, Read_buf.Length)
+                    Exit While
+
+                End If
+
+            Next
+
+
+            ReadIndex = ReadLen
+
+        End While
+
+
+
+        Return ReturnBuf
+
+
+    End Function
+
+
+
+
+
+
+
+
+
+End Class
