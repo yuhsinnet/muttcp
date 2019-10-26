30704658e521d12a594079f42deeb4a5ccc7484a
x78701542000 <x78701542000@livemail.tw>
2019-10-26 10:32:52 +0800

    onemore

---
+++ MUTTCP/OneMore/Mut.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Timers;
+    
+
+namespace OneMore
+{
+
+    class cli
+    {
+
+
+
+    }
+
+
+
+    class Mut
+    {
+        const int ClientLen = 3;
+
+        TcpClient Tcp_C;
+        TcpListener Tcp_S;
+
+        public Mut(string TGIP,int TGPort)
+        {
+
+
+
+        }
+
+
+
+    }
+}
